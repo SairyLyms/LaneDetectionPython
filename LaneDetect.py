80b10a97e3e348aeee7e219dc97ec2d8305d4ba2
Ryuma Saito <ryuma@RyumanoMacBook-Pro.local>
2018-06-10 21:49:43 +0900

    firstcommit

---
+++ LaneDetect.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import math
+
+
+
+def region_of_interest(img, vertices):
+
+    mask = np.zeros_like(img)   
+    
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+  
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=(255, 0, 0), thickness=7):
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros(img.shape, dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+def displayImage(img):
+    cv2.namedWindow("window")
+    cv2.imshow("window", img)
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
+
+
+
+def ReadVideoFrameBinary():
+    low_threshold = 100
+    high_threshold = 200
+    cap = cv2.VideoCapture('Sample.mp4')
+    if cap.isOpened():
+        rval , frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.Canny(frame, low_threshold, high_threshold)
+    displayImage(frame)
+    return frame
+
+
+ReadVideoFrameBinary()
